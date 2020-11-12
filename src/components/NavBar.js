61dc2317fbb61324d586f97f9590430fbf2426d5
Noah Franco <noahfranco@noahs-mbp.lan>
2020-11-12 13:43:30 -0500

    Made one last very small change to NavBar. Forgot to replace the localhost enpoints with the production endpoints on the NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,9 +12,9 @@ const NavBar = ({ onSelect, activeKey, ...props }) => {
         <Navbar.Body>
             <Nav onSelect={onSelect} activeKey={activeKey}>
             <Nav.Item eventKey="1" icon={<Icon icon="home" />}>
-            <a href="http://localhost:3000/"> Home </a>
+            <a href="https://tranquil-meadow-07587.herokuapp.com/"> Home </a>
             </Nav.Item>
-            <Nav.Item eventKey="2"> <a href="http://localhost:3000/newmovie"> Add Movie </a> </Nav.Item>
+            <Nav.Item eventKey="2"> <a href="https://tranquil-meadow-07587.herokuapp.com/newmovie"> Add Movie </a> </Nav.Item>
             </Nav>
         </Navbar.Body>
         </Navbar>
